83b6aa1b9b2ba97d687685c0dcbe3127b319b979
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-10-12 18:45:06 +0530

    Pagers Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Internships from './pages/Internships'
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App" >
+        <Router>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/internships">Internships</Link>
+
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/internships">
+              <Internships />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Internships/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Internships extends Component {
+  render() {
+    return (
+      <div>
+        Internships
+      </div>
+    )
+  }
+}
